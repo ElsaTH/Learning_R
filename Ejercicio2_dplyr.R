58d470d44ca2ee941c64b32d8cd05aad0c09b723
ElsaTH <elsatrb@gmail.com>
2020-09-27 13:57:41 +0200

    Ejercicio2 dplyr

--- Ejercicio2_dplyr.R
+++ Ejercicio2_dplyr.R
@@ -2,8 +2,7 @@
 
 library(msleep)
 library(ggplot2)
-file.choose()
-getwd()
+
  
 
 # Ejemplo para hacer el ejercicio:
@@ -17,22 +16,31 @@ ref <- ggplot(data, aes(x = bodywt, y = sleep_total))
 # This is the same thing as the line above:
 ref <- ggplot(data, aes(x = data$bodywt, y = data$sleep_total))
 
+#____________________________#
+# Par importar un archivo vamos a File >Import dataser >From text (readr)
+
+# Buscamos ruta de archivo
+file.choose()
+# Copiamos la ruta en la consola y la guardamos en una variable
+ruta_csv <- "/Users/elsa/Desktop/Lenguaje_R/R-basics/day3/data/sheeps.csv"
+
+
 
 
 #___________________________#
 # Ejercicio:
-func_sheep <- function(path="data/sheeps.csv", to_filter=FALSE){
+func_sheep <- function(path=ruta_csv, to_filter=FALSE){
   data <- read.csv(path, header = TRUE, sep = ",")
-  plot <- ggplot(data, aes(x=conservation)) + geom_line()
+  plot <- ggplot(data, aes(x=name, y=sleep_total)) + geom_line()
   
   if (to_filter == TRUE) {
-    new_data <- filter(data, conservation == "Primates")
+    new_data <- filter(data, order == "Carnivora")
       ggplot(aes(x=bodywt)) + geom_line()
   }
 }
 
 
-func_sheep(path="data/sheeps.csv")
+func_sheep(path=ruta_csv)
 
 #Se agrega color a la gráfica y una legenda
 plot + geom_point(aes(color = vore))
